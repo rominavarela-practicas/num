3d91ff3ed967980e78287cbe195953caadef48df
rominavarela <rominavarela.mx@gmail.com>
2016-06-13 13:35:22 -0500

    plot

--- interpolation/main.m
+++ interpolation/main.m
@@ -8,8 +8,8 @@ X = [0,8,16,24,32,40]';
 fX = [14.621,11.483,9.870,8.418,7.305,6.413]';
 granularity = 0.5;
 
-gX = interpolation_func(X,fX,1/8)
-x = linspace(X(1),X(end),numel(gX))
+gX = interpolation_func(X,fX,1/8);
+x = linspace(X(1),X(end),numel(gX));
 
 %% PLOT
 
@@ -18,6 +18,6 @@ xlabel('x');
 ylabel('y');
 
 plot(x,gX);
-plot(X,fX,'ro')
+plot(X,fX,'ro');
 
 hold off;
